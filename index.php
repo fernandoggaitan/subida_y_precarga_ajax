947d128806274ecb30f6e5a6b077a161113376a1
Fernando Gaitán <fernandoggaitan@gmail.com>
2014-02-20 17:28:45 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title> Subir archivos </title>
-        <script type="text/javascript" src="js/jquery-1.7.2.js"></script>
-        <script type="text/javascript" src="js/upload.js"></script>
+        <title> Subida y precarga ajax </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Css -->
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+
+        <!-- Javascript -->
+        <script src="js/jquery-2.0.2.js"></script>
+        <script src="js/upload.js"></script>
+        <script src="js/bootstrap.min.js"></script>
         <script type="text/javascript">
             function subirArchivos() {
                 $("#archivo").upload('subir_archivo.php',
-                        {
-                            nombre_archivo: $("#nombre_archivo").val()
-                        },
+                {
+                    nombre_archivo: $("#nombre_archivo").val()
+                },
                 function(respuesta) {
                     //Subida finalizada.
                     $("#barra_de_progreso").val(0);
                     if (respuesta === 1) {
-                        alert('El archivo ha sido subido correctamente.');
+                        mostrarRespuesta('El archivo ha sido subido correctamente.', true);
                         $("#nombre_archivo, #archivo").val('');
                     } else {
-                        alert('El archivo NO se ha podido subir.');
+                        mostrarRespuesta('El archivo NO se ha podido subir.', false);
                     }
                     mostrarArchivos();
                 }, function(progreso, valor) {
@@ -33,15 +39,15 @@
                     timeout: 10000,
                     data: {archivo: archivo},
                     error: function() {
-                        alert('Error al intentar eliminar el archivo.');
+                        mostrarRespuesta('Error al intentar eliminar el archivo.', false);
                     },
                     success: function(respuesta) {
                         if (respuesta == 1) {
-                            alert('El archivo ha sido eliminado.');
-                            mostrarArchivos();
+                            mostrarRespuesta('El archivo ha sido eliminado.', true);
                         } else {
-                            alert('Error al intentar eliminar el archivo.');
+                            mostrarRespuesta('Error al intentar eliminar el archivo.', false);                            
                         }
+                        mostrarArchivos();
                     }
                 });
             }
@@ -51,16 +57,24 @@
                     dataType: 'JSON',
                     success: function(respuesta) {
                         if (respuesta) {
-                            var lista = $("'<ul></ul>'");
+                            var html = '';
                             for (var i = 0; i < respuesta.length; i++) {
                                 if (respuesta[i] != undefined) {
-                                    lista.append('<li> <span> ' + respuesta[i] + ' </span> | <a class="eliminar_archivo" href="javascript:void(0);"> Eliminar </a> </li>');
+                                    html += '<div class="row"> <span class="col-lg-2"> ' + respuesta[i] + ' </span> <div class="col-lg-2"> <a class="eliminar_archivo btn btn-danger" href="javascript:void(0);"> Eliminar </a> </div> </div> <hr />';
                                 }
                             }
-                            $("#archivos_subidos").html(lista);
+                            $("#archivos_subidos").html(html);
                         }
                     }
                 });
+            }
+            function mostrarRespuesta(mensaje, ok){
+                $("#respuesta").removeClass('alert-success').removeClass('alert-danger').html(mensaje);
+                if(ok){
+                    $("#respuesta").addClass('alert-success');
+                }else{
+                    $("#respuesta").addClass('alert-danger');
+                }
             }
             $(document).ready(function() {
                 mostrarArchivos();
@@ -68,23 +82,44 @@
                     subirArchivos();
                 });
                 $("#archivos_subidos").on('click', '.eliminar_archivo', function() {
-                    var archivo = $(this).parents('li').find('span').text();
+                    var archivo = $(this).parents('.row').eq(0).find('span').text();
                     archivo = $.trim(archivo);
                     eliminarArchivos(archivo);
                 });
             });
         </script>
+
     </head>
     <body>
-        <form action="javascript:void(0);">
-            <label> Nombre el archivo: </label>
-            <input type="text" name="nombre_archivo" id="nombre_archivo" />
-            <input type="file" name="archivo" id="archivo" />
-            <br />
-            <input type="submit" id="boton_subir" value="Subir" />
-            <br />
-            <progress id="barra_de_progreso" value="0" max="100"></progress>
-        </form>
-        <div id="archivos_subidos"></div>
+        <div class="container">
+            <h1> Subida y precarga ajax  </h1>
+
+            <div id="respuesta" class="alert"></div>
+
+            <form action="javascript:void(0);">
+                <div class="row">
+                    <div class="col-lg-2"> 
+                        <label> Nombre el archivo: </label> 
+                    </div>
+                    <div class="col-lg-2">
+                        <input type="text" name="nombre_archivo" id="nombre_archivo" />
+                    </div>
+                    <div class="col-lg-2">
+                        <input type="file" name="archivo" id="archivo" />
+                    </div>                    
+                </div>
+                <hr />
+                <div class="row">
+                    <div class="col-lg-2">
+                        <input type="submit" id="boton_subir" value="Subir" class="btn btn-success" />
+                    </div>
+                    <div class="col-lg-4">
+                        <progress id="barra_de_progreso" value="0" max="100"></progress>
+                    </div>
+                </div>
+            </form>
+            <hr />
+            <div id="archivos_subidos"></div>
+        </div>
     </body>
 </html>
