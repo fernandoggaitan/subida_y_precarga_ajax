5fd6d8eb884f17b6f0c2866aad5b68bc3c944c5c
Fernando Gaitán <fernandoggaitan@gmail.com>
2014-02-20 17:28:21 -0200

    Update subir_archivo.php

--- subir_archivo.php
+++ subir_archivo.php
@@ -3,8 +3,8 @@
 if (isset($_FILES['archivo'])) {
     $archivo = $_FILES['archivo'];
     $extension = pathinfo($archivo['name'], PATHINFO_EXTENSION);
-    $nombre = "{$_POST['nombre_archivo']}.$extension";
-    $nombre = $nombre;
+	$time = time();
+    $nombre = "{$_POST['nombre_archivo']}_$time.$extension";
     if (move_uploaded_file($archivo['tmp_name'], "archivos_subidos/$nombre")) {
         echo 1;
     } else {
